b8f70853a62fa2dda7e7eef8f02dc2483e1e10d6
Debora Frias <deehconstantino@gmail.com>
2020-02-10 21:41:54 -0300

    Adicionando os itens de menu

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,4 +1,5 @@
 import { Restaurant } from './restaurant/restaurant.model'
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model'
 
 import { MEAT_API } from '../app.api'
 import { ErrorHandler } from '../app.error-handler'
@@ -28,5 +29,10 @@ export class RestaurantsService {
     reviewsOfRestaurant(id: string): Observable<any> {
       return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json())
       .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`).map(response => response.json())
+      .catch(ErrorHandler.handleError) 
     }
 }
